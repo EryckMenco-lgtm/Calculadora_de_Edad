8d6d0f6fe4194831fa457c85f13afaf5bb2fa617
Eryck Menco <mencoeryck@gmail.com>
2026-04-10 12:17:27 -0500

    primer cambio

---
+++ script.js
@@ -0,0 +1,52 @@
+const input_Fecha = document.getElementById("fechaNacimiento");
+const boton = document.getElementById("btnCalcular");
+const resultado = document.getElementById("resultado");
+
+boton.addEventListener("click", calcularEdad);
+
+function calcularEdad() {
+  
+  const fechaInput = input_Fecha.value;
+
+  if (fechaInput === "") {
+    resultado.innerText = "Llena los campos.";
+    return;
+  }
+
+  const fechaNacimiento = new Date(fechaInput);
+  const hoy = new Date();
+
+  if (fechaNacimiento > hoy) {
+    resultado.innerText = "La fecha no puede pasar de hoy.";
+    return;
+  }
+
+  const añoActual = hoy.getFullYear();
+  const añoNacimiento = fechaNacimiento.getFullYear();
+
+  let edad = añoActual - añoNacimiento;
+
+  const mesActual = hoy.getMonth();
+  const diaActual = hoy.getDate();
+
+  const mesNacimiento = fechaNacimiento.getMonth();
+  const diaNacimiento = fechaNacimiento.getDate();
+
+  if (
+    mesActual < mesNacimiento ||
+    (mesActual === mesNacimiento && diaActual < diaNacimiento)
+  ) {
+  }
+
+  let  mensaje = "";
+
+    if (edad < 18) {
+    mensaje += "Eres un menor.";
+  } else if (edad >= 18 && edad < 60) {
+    mensaje += "Estas viejo.";
+  } else {
+    mensaje += "Estas Muy viejo.";
+  }
+
+  resultado.innerText = "Tu tienes : " + edad + " años. " + mensaje;
+}
